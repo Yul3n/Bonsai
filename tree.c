c86288ad84eae680c38e71bd29d65e5ca9234e48
Yul3n <aghilas.boussaa@laposte.net>
2020-05-06 13:06:21 +0200

    Continued.

--- tree.c
+++ tree.c
@@ -1,10 +1,12 @@
 #include "config.h"
 #include <stdlib.h>
+#include <string.h>
 
 struct leaf {
 	char *name;
 	struct leaf *nodes;
 	int len; 
+	struct leaf *pred;
 };
 
 struct leaf 
@@ -12,7 +14,15 @@ add_node (struct leaf n, char *name)
 {
 	if (n.len) 
 		n.nodes = realloc(n.nodes, sizeof(struct leaf) * (n.len + 1));
-	else n.nodes = malloc(sizeof(struct leaf));
+	else {
+		n.nodes = malloc(sizeof(struct leaf));
+		n.len   = 0;
+	}
+	n.len ++;
+	n.nodes       += n.len;
+	n.nodes->len  = 0;
+	n.nodes->name = name;
+	n.pred        = &n;
 	return n;
 }
 
@@ -23,4 +33,9 @@ total_len (struct leaf n)
 	for (int i = 0; i < n.len; i++)
 		len += 1 + total_len(*(n.nodes + i));
 	return len;
+}
+
+struct leaf
+vmove_node (struct leaf n, int i)
+{
 }
